26809e369c8eb7375b9c0d093c0d6cf07d52b24c
CH3COOOH <happy_panda@126.com>
2023-03-29 23:23:45 +0900

    Change to sqlite3

--- lowestdb.py
+++ lowestdb.py
@@ -8,106 +8,127 @@ from azlib.pr import Log
 class LowestDB:
 	def __init__(self, columns, db_fname='lowtable.db', table_name='LOWTABLE', max_n=-99):
 		self.log = Log()
-		self.row_ctr = max_n
+		self.max_n = max_n
 		self.db_fname = db_fname
 		self.table_name = table_name
 		self.hash_map = {}
 		self.columns = columns  ## [(label1, dtype1), (label2, dtype2), ...]
-		# self.columns = self.__index_columns(columns)
-		if os.path.exists == False:
-			return self.__initDB()
+		if os.path.exists(db_fname) == False:
+			self.__initDB()
 	
-	def _initDB(self):
+	def __initDB(self):
 		sql_cmd = f'CREATE TABLE {self.table_name}('
-		isHead = True
+		sql_cmd += f'\nHASH TEXT PRIMARY KEY NOT NULL,'
 		for label, dtype in self.columns:
-			if isHead:
-				sql_cmd += f'\n{label} {dtype} PRIMARY KEY NOT NULL,'
-				isHead = False
-			else:
-				sql_cmd += f'\n{label} {dtype} NOT NULL,'
+			sql_cmd += f'\n{label} {dtype} NOT NULL,'
 		sql_cmd = sql_cmd[:-1] + ');'
 		print(sql_cmd)
 
-		# conn = sqlite3.connect(self.db_fname)
-		# c = conn.cursor()
-		# c.execute(sql_cmd)
-		# conn.commit()
-		# conn.close()
-		# self.log.print('New database created.')
-		# return 0
+		conn = sqlite3.connect(self.db_fname)
+		c = conn.cursor()
+		c.execute(sql_cmd)
+		conn.commit()
+		conn.close()
+		self.log.print('New database created.')
+		return 0
 
-	def _cmd_selectFromWhere(self, column_1, table, column_2, value):
-		if type(value) == str:
-			return f"SELECT {column_1} FROM {table} WHERE {column_2}='{value}'"
-		else:
-			return f"SELECT {column_1} FROM {table} WHERE {column_2}={value}"
+	def __cmd_selectXFromWhereYeq_(self, X, Y):
+		return f"SELECT {X} FROM {self.table_name} WHERE {Y}=?"
 
-	def _cmd_insertIntoValues(self, table, values):
+	def __cmd_insertIntoValues_(self):
 		cmd_values = ''
-		for i in range(len(values)):
-			if self.columns[i][1] == 'TEXT':
-				cmd_values += f"'{values[i]}',"
-			else:
-				cmd_values += f"{values[i]},"
-		return f"INSERT INTO {table} VALUES ({cmd_values[:-1]})"
+		for i in range(len(self.columns)+1):
+			cmd_values += '?,'
+		return f"INSERT INTO {self.table_name} VALUES ({cmd_values[:-1]})"
+	
+	def __cmd_deleteFromWhereXeq_(self, X):
+		return f"DELETE FROM {self.table_name} WHERE {X}=?"
 
-	def __isReachLimitation(self):
-		if self.row_ctr == 0:
+	def __isReachLimitation(self, c):
+		dbsize = list(c.execute(f"SELECT COUNT(*) FROM {self.table_name}"))[0][0]
+		if dbsize == self.max_n:
 			return True
 		else:
 			return False
-
-	def __isIDExisted(self, target_id, c):
-		if len(list(c.execute(__cmd_selectFromWhere('*', self.table_name, self.columns[0][0], target_id)))) != 0:
-			return True
-		else:
-			return False
-
-
 
 	def insert_row(self, hash_, values):
-		if self.__isReachLimitation():
-			return -0x01  ## --- Over max size
-		if len(values) != len(self.columns.keys()):
+		if len(values) != len(self.columns):
 			return -0x02  ## --- Different column amount
 
 		conn = sqlite3.connect(self.db_fname)
 		c = conn.cursor()
-		if self.__isIDExisted(hash_, c):
+		if self.__isReachLimitation(c):
 			conn.close()
-			return -0x03  ## --- Hash existed
+			return -0x01  ## --- Over max size
+		try:
+			hash_values = [hash_]
+			hash_values.extend(values)
+			c.execute(self.__cmd_insertIntoValues_(), hash_values)
+			conn.commit()
+		except sqlite3.IntegrityError:
+			conn.close()
+			return -0x03  ## --- Hash existed or something else
 
-
-		# if (hash_ in self.hash_map.keys()) == False:
-		# 	self.hash_map[hash_] = values
-
+		conn.close()
 		return 0
 
-			
-
-	def delete_row(self, hash_):
-		if hash_ in self.hash_map.keys():
-			del self.hash_map[hash_]
-			return 0
-		else:
+	def delete_row_by_hash(self, hash_):
+		conn = sqlite3.connect(self.db_fname)
+		c = conn.cursor()
+		if len(list(c.execute(self.__cmd_selectXFromWhereYeq_('HASH', 'HASH'), (hash_, )))) == 0:
+			conn.close()
 			return -0x04
+		c.execute(self.__cmd_deleteFromWhereXeq_('HASH'), (hash_, ))
+		conn.commit()
+		conn.close()
+		return 0
 
-	def extract_value(self, hash_, col):
-		if hash_ in self.hash_map.keys():
-			return self.hash_map[hash_][self.columns[col]]
-		else:
+	def extract_value_by_hash(self, hash_, col_name):
+		conn = sqlite3.connect(self.db_fname)
+		c = conn.cursor()
+		res = list(c.execute(self.__cmd_selectXFromWhereYeq_(col_name, 'HASH'), (hash_, )))
+		conn.close()
+		if len(res) == 0:
 			return None
+		else:
+			return res[0][0]
+	
+	def extract_row_by_hash(self, hash_):
+		conn = sqlite3.connect(self.db_fname)
+		c = conn.cursor()
+		res = list(c.execute(self.__cmd_selectXFromWhereYeq_('*', 'HASH'), (hash_, )))
+		conn.close()
+		if len(res) == 0:
+			return None
+		else:
+			return res[0]	
+	
+	def _inner_fun_test(self):
+		print(self.__cmd_select_FromWhere_eq_())
+		print(self.__cmd_insertIntoValues_())
+		print(self.__cmd_deleteFromWhere_eq_())
 
 
 
 if __name__ == '__main__':
-	ldb = LowestDB([('msg', 'TEXT'), ('timeout', 'INT')])
-	# print(ldb.insert_row('90da67dsfas2', ['fwqdq984dawds6ab==', '114514']))
-	# print(ldb.insert_row('80da6755as2s', ['48aff4fdawds64fa5a==', '114515']))
-	# print(ldb.extract_value('90da67dsfas2', 'msg'))
-	# ldb.delete_row('90da67dsfas2')
-	# print(ldb.extract_value('90da67dsfas2', 'msg'))
-	ldb._initDB()
-	print(ldb._cmd_selectFromWhere('*', ldb.table_name, ldb.columns[0][0], 'ac024f29'))
-	print(ldb._cmd_insertIntoValues(ldb.table_name, ['helllo world', 100]))
+	print('[Initiate DB]')
+	ldb = LowestDB(columns=[('MSG', 'TEXT'), ('TIMEOUT', 'INT')], max_n=-99)
+	print('[Insert]')
+	print(ldb.insert_row('90da67dsfas2', ['😀🥵', '114514']))
+	print(ldb.insert_row('80da6755as2s', ['🥰🤤', '114515']))
+	print(ldb.insert_row('8v9wedsjdfdge', ['🥰🤤', '114515']))
+	print(ldb.insert_row('90da67dsfas2', ['fwqdq984dawds6ab==', '114516']))
+	print('[Extract]')
+	print(ldb.extract_value_by_hash('90da67dsfas2', 'MSG'))
+	print('[Inject]')
+	print(ldb.extract_value_by_hash("*'", 'MSG'))
+	print('[Delete]')
+	print(ldb.delete_row_by_hash('90da67dsfas2'))
+	print(ldb.delete_row_by_hash('80da6755as2s'))
+	print(ldb.delete_row_by_hash('Unexist'))
+	print(ldb.extract_value_by_hash('90da67dsfas2', 'MSG'))
+#	print(ldb.insert_row('90da67dsfas2', ['😀🥵', '114514']))
+#	print(ldb.insert_row('80da6755as2s', ['🥰🤤', '114515']))
+#	print(ldb.insert_row('8v9wedsjdfdge', ['🥰🤤', '114515']))
+
+#	ldb._inner_fun_test()
